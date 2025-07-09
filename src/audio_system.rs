58318871ecdeccd1b1c48a30f219090a4257febd
Ian Gillespie <ian@flyingcouch.com>
2025-07-09 15:59:54 -0400

    sounds and music playing via rodio is now fully implimented and the extra bevy dependencies are GONE! furthermore, all audio files will be compiled into the exe, it's an all-in-one package

--- src/audio_system.rs
+++ src/audio_system.rs
@@ -4,29 +4,30 @@ use std::io::Cursor;
 
 #[derive(Resource)]
 pub struct MyAudio {
-    pub sound_sink: Sink,
+    pub audio_handle: OutputStreamHandle,
     pub music_sink: Sink
 } impl MyAudio {
     
-    pub fn new(handle: OutputStreamHandle) -> Self {
-        let sound_sink = Sink::try_new(&handle).unwrap();
-        let music_sink = Sink::try_new(&handle).unwrap();
+    pub fn new(audio_handle: OutputStreamHandle) -> Self {
+        let music_sink = Sink::try_new(&audio_handle).unwrap();
         Self {
-            sound_sink,
+            audio_handle,
             music_sink
         }
     }
     
     pub fn play_sound(&self, sound_data: &'static[u8]) {
+        let sink = Sink::try_new(&self.audio_handle).unwrap();
         let cursor = Cursor::new(sound_data);
         let source = Decoder::new(cursor).unwrap();
-        self.sound_sink.append(source);
+        sink.append(source);
+        sink.detach()
     }
     
     pub fn play_music(&self, sound_data: &'static[u8]) {
         self.music_sink.stop();
         let cursor = Cursor::new(sound_data);
         let source = Decoder::new(cursor).unwrap().repeat_infinite();
-        self.sound_sink.append(source);
+        self.music_sink.append(source);
     }
 }
--- src/dino_run_audio.rs
+++ src/dino_run_audio.rs
@@ -7,68 +7,35 @@ const HURT: &[u8] = include_bytes!("hurt.wav");
 const JUMP: &[u8] = include_bytes!("jump.wav");
 const SCORE: &[u8] = include_bytes!("score.wav");
 
-
-#[derive(Resource)]
-pub struct DinoSounds{
-    jump: Handle<AudioSource>,
-    hurt: Handle<AudioSource>,
-    score: Handle<AudioSource>
-}
-
 pub fn setup_audio(
-    asset_server: Res<AssetServer>,
-    mut commands: Commands,
     my_audio: Res<MyAudio>
 ) {
-    // commands.spawn((
-    //     AudioPlayer::new(asset_server.load("dino_sounds/dino_run_crystal.wav")),
-    //     PlaybackSettings::LOOP
-    // ));
     my_audio.play_music(SOUND_TRACK);
-    commands.insert_resource(
-        DinoSounds {
-            jump: asset_server.load("dino_sounds/jump.wav"),
-            hurt: asset_server.load("dino_sounds/hurt.wav"),
-            score: asset_server.load("dino_sounds/score.wav")
-        }
-    );
 }
 
 pub fn jump_audio(
-    dino_sounds: Res<DinoSounds>,
     mut event_reader: EventReader<PlayerJumps>,
-    mut commands: Commands
+    my_audio: Res<MyAudio>
 ) {
     for _ in event_reader.read(){
-        commands.spawn((
-            AudioPlayer::new(dino_sounds.jump.clone()),
-            PlaybackSettings::DESPAWN
-        ));
+        my_audio.play_sound(JUMP);
     };
 }
 
 pub fn score_audio(
-    dino_sounds: Res<DinoSounds>,
     mut event_reader: EventReader<PlayerScores>,
-    mut commands: Commands
+    my_audio: Res<MyAudio>
 ) {
     for _ in event_reader.read(){
-        commands.spawn((
-            AudioPlayer::new(dino_sounds.score.clone()),
-            PlaybackSettings::DESPAWN
-        ));
+        my_audio.play_sound(SCORE);
     };
 }
 
 pub fn hurt_audio(
-    dino_sounds: Res<DinoSounds>,
     mut event_reader: EventReader<PlayerHurt>,
-    mut commands: Commands
+    my_audio: Res<MyAudio>
 ) {
     for _ in event_reader.read(){
-        commands.spawn((
-            AudioPlayer::new(dino_sounds.hurt.clone()),
-            PlaybackSettings::DESPAWN
-        ));
+        my_audio.play_sound(HURT);
     };
 }
