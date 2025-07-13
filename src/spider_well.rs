efde2c64c3e901fb4e82040ce8fe49a7d406833e
Ian Gillespie <ian@flyingcouch.com>
2025-07-13 19:45:32 -0400

    we have... rendered thread!!

--- src/spider_well.rs
+++ src/spider_well.rs
@@ -2,16 +2,20 @@ use bevy::prelude::*;
 pub mod mechanics;
 
 use mechanics::{
-    debug_scene_setup, spawn_player, insert_resources, player_controls, move_player
+    debug_scene_setup, spawn_player, insert_simple_resources, player_controls, move_player,
+    insert_webbing_assets, web_spawner, web_updater
 };
 
 pub struct SpiderWellPlugin;
 impl Plugin for SpiderWellPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, debug_scene_setup);
-        app.add_systems(Startup, insert_resources);
-        app.add_systems(Startup, spawn_player);
+        app.add_systems(Startup, insert_simple_resources);
+        app.add_systems(Startup, insert_webbing_assets);
+        app.add_systems(Startup, spawn_player.after(insert_webbing_assets));
         app.add_systems(PreUpdate, player_controls);
         app.add_systems(Update, move_player);
+        app.add_systems(PreUpdate, web_spawner);
+        app.add_systems(Update, web_updater);
     }
 }
--- src/spider_well/mechanics.rs
+++ src/spider_well/mechanics.rs
@@ -2,6 +2,8 @@ use std::f32::consts::{FRAC_PI_2, TAU};
 use bevy::{core_pipeline::{bloom::Bloom, tonemapping::Tonemapping}, prelude::*, render::camera::ScalingMode};
 
 const THREAD_LENGTH_START: f32 = 3.0;
+const THREAD_RADIUS: f32 = 0.125;
+const STICKING_POINT_RADIUS: f32 = 0.2;
 const PLAYER_RADIUS: f32 = 0.5;
 const LEVEL_WIDTH: f32 = 20.0;
 const GRAVITY: f32 = 0.75;
@@ -10,6 +12,7 @@ const PLAYER_LEAN: f32 = 0.05;
 const PLAYER_CLIMB: f32 = 2.0;
 const PLAYER_DRAG: f32 = 0.10;
 const STATIC_DRAG: f32 = 1.0;
+const ASSUMED_STICKING_POINTS: usize = 10;
 
 #[derive(Component)]
 pub struct POVCamera;
@@ -50,11 +53,11 @@ pub struct PlayerMarker;
 pub fn spawn_player(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    webbing_assets: Res<WebbingAssets>
 ) {
     let mat = materials.add(StandardMaterial {
-        base_color: Color::linear_rgba(0.8, 0.0, 0.0, 0.2),
-        alpha_mode: AlphaMode::Add,
+        base_color: Color::linear_rgba(0.8, 0.0, 0.0, 1.0),
         unlit: true,
         ..default()
     });
@@ -65,22 +68,32 @@ pub fn spawn_player(
         Transform::default(),
         PlayerMarker
     ));
+    let thread = commands.spawn((
+        Mesh3d(webbing_assets.thread_mesh.clone()),
+        MeshMaterial3d(webbing_assets.material.clone()),
+        Transform::default(),
+        ThreadMarker
+    )).id();
+    commands.insert_resource(PlayerThread{entity: thread});
 }
 
-pub fn insert_resources(
+pub fn insert_simple_resources(
     mut commands: Commands,
 ) {
     commands.insert_resource(PlayerPos{vec: Vec2::ZERO});
     commands.insert_resource(PlayerSwing{thread_length: THREAD_LENGTH_START, angle: 0.0, angular_v: 0.0});
     commands.insert_resource(StickingPoints{vec: {
-        let mut v = Vec::with_capacity(10);
+        let mut v = Vec::with_capacity(ASSUMED_STICKING_POINTS);
         v.push(Vec2::new(0.0, THREAD_LENGTH_START));
         v
     }});
+    commands.insert_resource(StickingSpheres{vec: Vec::with_capacity(ASSUMED_STICKING_POINTS)});
+    commands.insert_resource(StickingThreads{vec: Vec::with_capacity(ASSUMED_STICKING_POINTS-1)});
     commands.insert_resource(PlayerInputs{
         x: 0, y: 0, leaping: false, scheme: ControlScheme::Wasd
     });
     commands.insert_resource(PlayerVelocity {vec: Vec2::ZERO});
+    commands.insert_resource(UpdateStaticThreads{bool: true});
 }
 
 #[derive(Eq, PartialEq, Copy, Clone)]
@@ -185,7 +198,7 @@ pub fn move_player(
         let delta_vec = stuck - player_pos.vec;
         player_swing.angle = -delta_vec.x.atan2(delta_vec.y);
         player_transform.translation = player_pos.vec.extend(0.0);
-        return;
+        return;  // TODO: improve the way leaping velocity is added to swinging velocity
     };
 
     // rope-crawl
@@ -230,4 +243,129 @@ pub fn move_player(
     
     //update translation
     player_transform.translation = player_pos.vec.extend(0.0);
+}
+
+#[derive(Resource)]
+pub struct WebbingAssets {
+    material: Handle<StandardMaterial>,
+    thread_mesh: Handle<Mesh>,
+    sticky_mesh: Handle<Mesh>
+} impl WebbingAssets {
+    pub fn new(
+        material: Handle<StandardMaterial>, thread_mesh: Handle<Mesh>, sticky_mesh: Handle<Mesh>
+    ) -> Self {
+        Self {material, thread_mesh, sticky_mesh}
+    }
+    pub fn get_material(&self) -> Handle<StandardMaterial> {
+        self.material.clone()
+    }
+    pub fn get_thread(&self) -> Handle<Mesh> {
+        self.thread_mesh.clone()
+    }
+    pub fn get_sticky(&self) -> Handle<Mesh> {
+        self.thread_mesh.clone()
+    }
+}
+
+pub fn insert_webbing_assets(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>
+) {
+    let material = materials.add(StandardMaterial {
+        base_color: Color::WHITE,
+        perceptual_roughness: 0.0,
+        reflectance: 1.0,
+        clearcoat: 0.0,
+        clearcoat_perceptual_roughness: 0.0,
+        anisotropy_rotation: 0.0,
+        anisotropy_strength: 1.0,
+        ..default()
+    });
+    let thread_mesh = meshes.add(Cylinder::new(THREAD_RADIUS, 1.0));
+    let sticky_mesh = meshes.add(Sphere::new(STICKING_POINT_RADIUS));
+    commands.insert_resource(WebbingAssets::new(material, thread_mesh, sticky_mesh));
+}
+
+#[derive(Resource)]
+pub struct StickingSpheres {
+    vec: Vec<Entity>
+}
+
+#[derive(Resource)]
+pub struct StickingThreads {
+    vec: Vec<Entity>
+}
+
+#[derive(Component)]
+pub struct ThreadMarker;
+
+#[derive(Component)]
+pub struct StickingPointMarker;
+
+pub fn web_spawner(
+    sticking_points: Res<StickingPoints>,
+    webbing_assets: Res<WebbingAssets>,
+    mut commands: Commands,
+    mut sticking_spheres: ResMut<StickingSpheres>,
+    mut sticking_threads: ResMut<StickingThreads>,
+    mut update_threads: ResMut<UpdateStaticThreads>
+) {
+    while sticking_points.vec.len() > sticking_spheres.vec.len() {
+        let i = sticking_spheres.vec.len();
+        let point = sticking_points.vec[i];
+        let entity = commands.spawn((
+            Mesh3d(webbing_assets.sticky_mesh.clone()),
+            MeshMaterial3d(webbing_assets.material.clone()),
+            Transform::from_translation(point.extend(0.0)),
+            StickingPointMarker
+        )).id();
+        sticking_spheres.vec.push(entity);
+        if sticking_spheres.vec.len() > 1 {
+            let entity = commands.spawn((
+                Mesh3d(webbing_assets.thread_mesh.clone()),
+                MeshMaterial3d(webbing_assets.material.clone()),
+                Transform::default(),
+                ThreadMarker
+            )).id();
+            sticking_threads.vec.push(entity);
+            update_threads.bool = true;
+        };
+    };
+    while sticking_points.vec.len() < sticking_spheres.vec.len() {
+        let entity = sticking_spheres.vec.pop().unwrap();
+        commands.entity(entity).despawn();
+        let entity = sticking_threads.vec.pop().unwrap();
+        commands.entity(entity).despawn();
+        update_threads.bool = true;
+    };
+}
+
+#[derive(Resource)]
+pub struct UpdateStaticThreads {
+    bool: bool
+}
+
+#[derive(Resource)]
+pub struct PlayerThread {
+    entity: Entity
+}
+
+pub fn web_updater(
+    sticking_points: Res<StickingPoints>,
+    threads: Res<StickingThreads>,
+    player_thread: Res<PlayerThread>,
+    player_pos: Res<PlayerPos>,
+    player_swing: Res<PlayerSwing>,
+    update_threads: Res<UpdateStaticThreads>,
+    mut query: Query<&mut Transform, With<ThreadMarker>>
+) {
+    let mut player_thread_transform = query.get_mut(player_thread.entity).unwrap();
+    let average_pos = (player_pos.vec + sticking_points.vec.last().unwrap()) / 2.0;
+    player_thread_transform.translation = average_pos.extend(0.0);
+    player_thread_transform.scale.y = player_swing.thread_length;
+    player_thread_transform.rotation = Quat::from_rotation_z(player_swing.angle);
+    if update_threads.bool {
+        
+    };
 }
